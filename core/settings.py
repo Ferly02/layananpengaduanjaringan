688e7630f1ad83a71e76e4d0c02da38d4929907d
ferly02 <ferlyardiansyah02@gmail.com>
2026-08-05 09:39:11 +0700

    Fix PyMySQL ssl_mode bug by falling back to native mysqlclient

--- core/settings.py
+++ core/settings.py
@@ -13,8 +13,11 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 import os
 import dj_database_url
 from pathlib import Path
-import pymysql
-pymysql.install_as_MySQLdb()
+try:
+    import MySQLdb
+except ImportError:
+    import pymysql
+    pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
